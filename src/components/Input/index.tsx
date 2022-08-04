99befa18ffdb81e405c5018fb4eb862852c2535e
Walter Ribeiro <wribeiro@testreach.com>
2022-08-04 13:59:26 -0300

    Finish challenge

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,22 @@
-import {
+import React, {
   useEffect,
   useRef,
   useState,
   useCallback,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: any;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
---
+++ src/components/ModalAddFood/index.tsx
@@ -0,0 +1,42 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+import { IFood } from '../../pages/Dashboard';
+
+interface Props {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleAddFood: (food: IFood) => void;
+}
+
+export default function ModalAddFood({ isOpen, setIsOpen, handleAddFood }: Props) {
+  const formRef = useRef(null)
+
+  async function handleSubmit(data: IFood) {
+    handleAddFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
